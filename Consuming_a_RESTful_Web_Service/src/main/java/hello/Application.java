1e5303d86a4cfbe01eba09352a64d30405234067
Zz-m <adjim234@gmail.comm>
2017-02-16 16:39:57 +0800

    完成 Consuming a RESTful Web Service training

--- Consuming_a_RESTful_Web_Service/src/main/java/hello/Application.java
+++ Consuming_a_RESTful_Web_Service/src/main/java/hello/Application.java
@@ -2,18 +2,35 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Application
  * Created by dhx on 2017/2/16.
  */
+@SpringBootApplication
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String args[]) {
-        RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        log.info(quote.toString());
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info("--------------------");
+            log.info(quote.toString());
+        };
     }
 }
