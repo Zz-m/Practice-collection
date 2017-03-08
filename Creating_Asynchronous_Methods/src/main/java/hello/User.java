a489d7eb2c7ed6dab9230891ebb6a8cbaea323d0
Zz-m <adjim234@gmail.comm>
2017-03-08 15:32:25 +0800

    完成 Creating Asynchronous Methods Spring的异步方法框架，例子有点简单

---
+++ Creating_Asynchronous_Methods/src/main/java/hello/User.java
@@ -0,0 +1,41 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class User {
+
+    private String name;
+    private String blog;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", blog=" + blog + ", url=" + url + "]";
+    }
+
+}
