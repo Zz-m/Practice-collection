d4beeafe90a3d351ab33e3ec3264efab3e7fdb9e
zz-m <adjim234@gmail.com>
2019-06-13 19:01:05 +0800

    command pattern

---
+++ designPattern/src/main/java/com/adj/command/Command.java
@@ -0,0 +1,5 @@
+package com.adj.command;
+
+public interface Command {
+    public void execute();
+}
---
+++ designPattern/src/main/java/com/adj/command/Light.java
@@ -0,0 +1,7 @@
+package com.adj.command;
+
+public class Light {
+    public void on() {
+        System.out.println("I am light, I am on.");
+    }
+}
