dc6e0883f57af9a4d18e33741ee270779e80a921
zz-m <adjim234@gmail.com>
2019-07-31 20:34:57 +0800

    wechat app

---
+++ wechatApp/startProject/pages/testPage/testPage.js
@@ -0,0 +1,81 @@
+// pages/testPage/testPage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    user: {
+      name: "asd",
+      age: 123
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  changeName: function (e) {
+    console.log(this.data.user.name);
+    console.log(this.data.user.age);
+    // let user = this.data.user;
+    // console.log(user.name);
+    // console.log(user.age);
+    // user.name = "denghanxi";
+    this.setData({
+      "user.name": "denghanxi"
+    })
+  }
+})
