1b276da805e1872ca06e408734e697d5a3e9d2eb
Zz-m <adjim234@gmail.com>
2020-09-16 18:16:20 +0800

    finish angular material

--- angular-start/material-demo/src/app/material/material.module.ts
+++ angular-start/material-demo/src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
 
-const MaterialComponents = [MatButtonModule, MatIconModule]
+const MaterialComponents = [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 
 @NgModule({
   declarations: [],
