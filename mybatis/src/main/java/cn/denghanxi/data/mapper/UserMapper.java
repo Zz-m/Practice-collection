9f12164f5b1faa89adb9dc43e851de3d1046ffc7
Zz-m <adjim234@gmail.com>
2020-09-29 22:43:24 +0800

    create user-role system mappers

---
+++ mybatis/src/main/java/cn/denghanxi/data/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package cn.denghanxi.data.mapper;
+
+import cn.denghanxi.model.SysUser;
+
+public interface UserMapper {
+    SysUser selectById(Long id);
+}
---
+++ mybatis/src/main/java/cn/denghanxi/model/SysRole.java
@@ -0,0 +1,62 @@
+package cn.denghanxi.model;
+
+import java.util.Date;
+
+public class SysRole {
+    private Long id;
+    private String roleName;
+    private boolean enabled;
+    private Long createBy;
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Long getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Long createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "SysRole{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                ", enabled=" + enabled +
+                ", createBy=" + createBy +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
