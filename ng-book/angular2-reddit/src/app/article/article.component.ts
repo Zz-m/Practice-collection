c37a4250bd300f496774a23bbf091e40e5d3ca56
dhx <adjim234@gmail.com>
2021-12-23 15:56:13 +0800

    p39

--- ng-book/angular2-reddit/src/app/article/article.component.ts
+++ ng-book/angular2-reddit/src/app/article/article.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Article} from "./article.model";
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
+  article: Article;
 
-  constructor() { }
+  constructor() {
+    this.article = new Article('Angular 2', 'https://angular.io', 10)
+  }
 
   ngOnInit(): void {
   }
+
+  voteUp() {
+    this.article.voteUp() ;
+    return false
+  }
+
+  voteDown() {
+    this.article.voteDown()
+    return false;
+  }
+}
+
+class John {
 
 }
