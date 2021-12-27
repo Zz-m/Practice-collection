5b63e2a550973f65e573f94cb7b02e3efca16557
dhx <adjim234@gmail.com>
2021-12-27 17:39:50 +0800

    p112

--- ng-book/inventory-app/src/app/app.component.ts
+++ ng-book/inventory-app/src/app/app.component.ts
@@ -9,34 +9,13 @@ import {Product} from "./model/Product";
 export class AppComponent {
     title = 'inventory-app';
 
-    products: Product[];
-
-
     constructor() {
-        this.products = [
-            new Product(
-                'MYSHOES',
-                'Black Running Shoes',
-                '/resources/images/products/black-shoes.jpg',
-                ['Men', 'Shoes', 'Running Shoes'],
-                109.99),
-            new Product(
-                'NEATOJACKET',
-                'Blue Jacket',
-                '/resources/images/products/blue-jacket.jpg',
-                ['Women', 'Apparel', 'Jackets & Vests'],
-                238.99),
-            new Product(
-                'NICEHAT',
-                'A Nice Black Hat',
-                '/resources/images/products/black-hat.jpg',
-                ['Men', 'Accessories', 'Hats'],
-                29.99)
-        ];
+
     }
 
-    productWasSelected(product: Product): void {
-        console.log("product clicked: ", product)
+
+    onSubmit(value: any) {
+        console.log("submit value:",value);
     }
 }
 
--- ng-book/inventory-app/src/app/app.module.ts
+++ ng-book/inventory-app/src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { RowItemComponent } from './component/row-item/row-item.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {RowItemComponent} from './component/row-item/row-item.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    RowItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        RowItemComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
