16a5177bd30cda7273d0dfbbc6ca809e9e623321
dhx <adjim234@gmail.com>
2022-10-20 09:21:39 +0800

    升级到rxjava3

--- rxJava/src/main/java/com/adj/Hello.java
+++ rxJava/src/main/java/com/adj/Hello.java
@@ -1,11 +1,8 @@
 package com.adj;
 
-import io.reactivex.Flowable;
-import io.reactivex.disposables.Disposable;
 
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        Disposable disposable = Flowable.fromArray(args).subscribe(s -> System.out.println("Hello" + s));
     }
 }
