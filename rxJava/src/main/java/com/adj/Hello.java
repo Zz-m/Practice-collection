ad368a4c795d88205119a88c01ba86d8e3404be8
dhx <adjim234@gmail.com>
2023-04-24 14:58:30 +0800

    test Observable

--- rxJava/src/main/java/com/adj/Hello.java
+++ rxJava/src/main/java/com/adj/Hello.java
@@ -11,6 +11,20 @@ public class Hello {
             s.onComplete();
         }).subscribe(System.out::println);
 
+        Observable<Integer> o1 = Observable.just(1, 2, 3);
+
+        Observable<Boolean> o2 = o1.concatMap(i -> {
+            if (i == 1) {
+                return Observable.just(true);
+            } if (i == 3) {
+                return Observable.just(true);
+            } else {
+                return Observable.empty();
+            }
+        });
+
+        o2.subscribe(System.out::println);
+
     }
 
 }
