2c6790c4e8f7a1cb265da4f4f4f29cc17a5a413a
dhx <adjim234@gmail.com>
2023-10-12 15:54:59 +0800

    chore

--- c-sharp-intro/tutorial/math-intro/Program.cs
+++ c-sharp-intro/tutorial/math-intro/Program.cs
@@ -14,6 +14,11 @@ namespace math_intro
             
             Console.WriteLine(p1);
             Console.WriteLine(p2 == null);
+
+            Whatever whatever = new();
+            var (age, name) = whatever;
+            Console.WriteLine(age);
+            Console.WriteLine(name);
         }
     }
 
@@ -24,7 +29,7 @@ namespace math_intro
 
         public Person(int age, string name)
         {
-            this._age = age;
+            _age = age;
             this._name = name;
         }
 
@@ -55,5 +60,13 @@ namespace math_intro
         {
             throw new NotImplementedException();
         }
+
+        public void Deconstruct(out int age, out string name)
+        {
+            age = 12;
+            name = "John";
+        }
+
     }
+
 }
